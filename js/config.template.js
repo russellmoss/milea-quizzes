46bc542407e231fe4cd26f1e513eb793683d85cc
Russsell <russell@mileaestatevineyard.com>
2025-05-25 14:28:20 -0400

    Add config template and update .gitignore to exclude sensitive files

---
+++ js/config.template.js
@@ -0,0 +1,10 @@
+// config.template.js
+// Copy this file to config.js and replace the placeholder values with your Firebase config
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+}; 
